abe6f830486a522b46f573f3bd9b4c293a2ad160
JigangGuo <707394840@qq.com>
2023-10-06 17:22:12 +1300

    development

--- modules/custom/my_simple_module/src/Controller/MySimpleController.php
+++ modules/custom/my_simple_module/src/Controller/MySimpleController.php
@@ -5,25 +5,36 @@ namespace Drupal\my_simple_module\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\my_simple_module\Service\MySimpleService;
+use Drupal\my_simple_module\Service\AnotherService;
 
 class MySimpleController extends ControllerBase {
 
-  protected $mySimpleService;
+  protected $mySimpleService1;
+  protected $mySimpleService2;
 
-  public function __construct(MySimpleService $mySimpleService) {
-    $this->mySimpleService = $mySimpleService;
+  public function __construct(MySimpleService $mySimpleService1, AnotherService $mySimpleService2) {
+    $this->mySimpleService1 = $mySimpleService1;
+    $this->mySimpleService2 = $mySimpleService2;
   }
 
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('my_simple_module.service')
+      $container->get('my_simple_module.service1'),
+      $container->get('my_simple_module.service2')
     );
   }
 
   public function content() {
     return [
-      '#markup' => $this->mySimpleService->getMessage(),
+      '#markup' => $this->mySimpleService1->getMessage(),
     ];
   }
+
+  public function myCustomMethod() {
+    return [
+      '#markup' => $this->mySimpleService2->getMessage(),
+    ];
+  }
+  
 
 }
---
+++ modules/custom/my_simple_module/src/Service/AnotherService.php
@@ -0,0 +1,7 @@
+<?php
+namespace Drupal\my_simple_module\Service;
+class AnotherService {
+  public function getMessage() {
+    return 'Hello from AnotherService xxxxxxxxxx!';
+  }
+}
